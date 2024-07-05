95c9046faa33aced62f48d0f45c94453f316c3ec
Ryan Atkinson <mail@ryanatkn.com>
2024-07-05 09:01:46 -0600

    fix selected link

--- src/routes/Community_Links.svelte
+++ src/routes/Community_Links.svelte
@@ -3,13 +3,13 @@
 		<a rel="me" href="https://discord.gg/YU5tyeK72X">Discord</a>
 	</li>
 	<li>
-		<a rel="me" href="https://www.spiderspace.org/" class="selected">spiderspace.org</a> -
-		nontechnical design videos where we'll make a social website together with
+		<a rel="me" href="https://www.spiderspace.org/">spiderspace.org</a> - nontechnical design videos
+		where we'll make a social website together with
 		<a href="https://zzz.ryanatkn.com/">Zzz</a> when it's ready
 	</li>
 	<li>
-		<a rel="me" href="https://www.webdevladder.net/">webdevladder.net</a> - realworld webdev with TypeScript
-		and Svelte, you are here
+		<a rel="me" href="https://www.webdevladder.net/" class="selected">webdevladder.net</a> - realworld
+		webdev with TypeScript and Svelte, you are here
 	</li>
 	<li>
 		<a rel="me" href="https://www.ryanatkn.com/">ryanatkn.com</a> - my homepage
