c26d9bd3e6a0a672945061ef68787cc210bdee20
Ryan Atkinson <mail@ryanatkn.com>
2024-07-10 07:07:25 -0600

    wip

---
+++ src/routes/video_data.ts
@@ -0,0 +1,23 @@
+import type {Video_Item} from '$routes/video.js';
+
+export const webdevladder_videos: Video_Item[] = [
+	{
+		title: 'How Svelte runes syntax is better than plain JavaScript with signals',
+		url: 'https://www.youtube.com/watch?v=IsJtmbvW2SI',
+	},
+];
+
+export const webdevladder_vods_videos: Video_Item[] = [
+	{
+		title: 'Svelte 5 refactoring to reactive SvelteSet in my ear trainer Earbetter',
+		url: 'https://www.youtube.com/watch?v=bE-BYtepFr8',
+	},
+	{
+		title: 'Embedding Mastodon toots with rules to control reply visibility with Svelte components',
+		url: 'https://www.youtube.com/watch?v=5w9iUZ5TesE',
+	},
+	{
+		title: 'Refactoring my messy Mastodon library',
+		url: 'https://www.youtube.com/watch?v=QET2GbcW9ww',
+	},
+];
