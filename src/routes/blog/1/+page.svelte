d833b8aa50ce21bd1d65b1cbc87ef9bc85086ee1
Ryan Atkinson <mail@ryanatkn.com>
2024-07-10 16:22:00 -0600

    wip

--- src/routes/blog/1/+page.svelte
+++ src/routes/blog/1/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" context="module">
 	import type {Blog_Post_Data} from '@ryanatkn/fuz_blog/blog.js';
+	import Blog_Post from '@ryanatkn/fuz_blog/Blog_Post.svelte';
 
 	export const post = {
 		title: 'Hello webdevladder blog',
@@ -12,9 +13,30 @@
 </script>
 
 <script lang="ts">
-	import {base} from '$app/paths';
+	//
 </script>
 
-<p>
-	<a href="{base}/a/b/c">todo a local link example</a>
-</p>
+<Blog_Post {post}>
+	<p>
+		This blog was created with <code>fuz_blog</code>, a SvelteKit library for making blogs for
+		developers. (<a href="https://blog.fuz.dev">blog.fuz.dev</a>,
+		<a href="https://github.com/ryanatkn/fuz_blog">source code</a>)
+	</p>
+	<p>
+		If you're interested in the full videos where I made the software, extracting it from my
+		personal blog:
+	</p>
+	<ul>
+		<li>
+			<a href="https://www.youtube.com/watch?v=wP9qghWkTLc"
+				>Coding a blog creation library for SvelteKit developers from scratch</a
+			>
+		</li>
+		<li>
+			<a href="https://www.youtube.com/watch?v=dh_6Vyjbuiw"
+				>Extracting fuz_blog from my personal blog and publishing to npm</a
+			>
+		</li>
+	</ul>
+	<p>More to come!</p>
+</Blog_Post>
