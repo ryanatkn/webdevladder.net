6ce65661a6d6cdad2d4c037b608c551ae47edacb
Ryan Atkinson <mail@ryanatkn.com>
2025-06-27 11:25:00 -0400

    deployment

--- _app/immutable/nodes/10.BkAUQL0k.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/FM2UfGgQ.js";import{component as p}from"./7.CCFJ6kpq.js";function e(o){p(o)}export{e as component};
--- _app/immutable/nodes/7.BvPgEvXX.js
+++ _app/immutable/nodes/7.BvPgEvXX.js
@@ -1,4 +1,4 @@
-import{t as p,a as c}from"../chunks/FM2UfGgQ.js";import{f as m,s as n,c as a,P as s,r}from"../chunks/Dwe2blsr.js";import{s as h}from"../chunks/bjXBF-qO.js";import{b as u}from"../chunks/6RuBs-83.js";import{B as f}from"../chunks/BL840WsL.js";const y={title:"Introducing webdevladder",slug:"introducing-webdevladder",date_published:"2024-08-14T18:10:26.092Z",date_modified:"2025-06-27T14:22:22.080Z",summary:"I describe the purpose of my YouTube channel and blog, webdevladder, and my plans for the future",tags:["webdevladder"],comments:{type:"mastodon",url:"https://fosstodon.org/@webdevladder/113312132188029470"}};var w=p(`<section><p>Webdevladder is a <a href="https://youtube.com/@webdevladder">YouTube channel</a> and <a href="https://www.webdevladder.net/blog">blog</a> about the technical topics that interest
+import{t as p,a as m}from"../chunks/FM2UfGgQ.js";import{f as c,s as n,c as a,P as s,r}from"../chunks/Dwe2blsr.js";import{s as h}from"../chunks/bjXBF-qO.js";import{b as u}from"../chunks/BSY4quHh.js";import{B as f}from"../chunks/BL840WsL.js";const y={title:"Introducing webdevladder",slug:"introducing-webdevladder",date_published:"2024-08-14T18:10:26.092Z",date_modified:"2025-06-27T15:24:37.363Z",summary:"I describe the purpose of my YouTube channel and blog, webdevladder, and my plans for the future",tags:["webdevladder"],comments:{type:"mastodon",url:"https://fosstodon.org/@webdevladder/113312132188029470"}};var w=p(`<section><p>Webdevladder is a <a href="https://youtube.com/@webdevladder">YouTube channel</a> and <a href="https://www.webdevladder.net/blog">blog</a> about the technical topics that interest
 			me as I make open source software. I've been a professional web developer since 2011, and
 			since 2019 I've been working in public full-time, publishing most of my code to <a href="https://github.com/ryanatkn">GitHub</a> as I write it. Webdevladder is a public alias
 			that feels comfortably decoupled from my personal life (being on GitHub since 2012 with my real
@@ -13,10 +13,11 @@ import{t as p,a as c}from"../chunks/FM2UfGgQ.js";import{f as m,s as n,c as a,P a
 			from a CSS framework to a Svelte UI library to a fullstack dev-and-end-user tool. I also make
 			websites that use the stack and tools to streamline the process. The fullstack tool is <a href="https://www.zzz.software/">Zzz</a>, and it's my focus for funding because its target
 			audience is both nontechnical and dev users, and it's the main thing my other software has
-			been designed for.</p> <p>My goal here is to deepen my knowledge of web technology and improve my communication skills
-			through blogging and videos. You're reading the blog now, and I'm producing educational videos
-			on my <a href="https://youtube.com/@webdevladder">main channel</a>, and less polished content
-			like raw programming sessions and demos of my apps on a <a href="https://youtube.com/@webdevladder_vods">second channel</a>.</p> <p>But before you subscribe to <a>the feed</a>, here's some more
+			been designed for.</p> <p>My stated goal here is to deepen my knowledge of web technology and improve my communication
+			skills through blogging and videos, and an ulterior motive is to market my work for
+			sustainability and egoic self interest. You're reading the blog now, and I'm producing
+			educational videos on my <a href="https://youtube.com/@webdevladder">main channel</a>, and
+			less polished content like raw programming sessions and demos of my apps on a <a href="https://youtube.com/@webdevladder_vods">second channel</a>.</p> <p>But before you subscribe to <a>the feed</a>, here's some more
 			context.</p></section> <section><h2>What Webdevladder is not</h2> <p>I'm seeking to be an independent open source developer funded directly by my users, which is
 			an uncommon situation in 2024. I hope that the few user-funded developers of today will be
 			joined by many more in the future, and I want to be among them. I like the idea of having many
@@ -45,7 +46,7 @@ import{t as p,a as c}from"../chunks/FM2UfGgQ.js";import{f as m,s as n,c as a,P a
 			that's excellently covered elsewhere, and I won't sell courses or do any time-consuming
 			content creation for media companies. While Webdevladder is educational content, it's tailored
 			to what I find interesting and generally more advanced and less accessible. I don't run a
-			Discord community and I'm focused on my projects, but I would like to, so to speak, prop up a
+			Discord community and I'm focused on my projects, but I would like, so to speak, to prop up a
 			ladder that anyone could climb.</p></section> <section><h2>My plan</h2> <p>I'm lucky to have the resources to pursue my <a href="https://www.ryanatkn.com/tree">projects</a> for a few more years, and regardless of my personal situation I plan to continue working on this
 			software and monetize it without possibly-enshittifying patterns. It's all permissively licensed
 			so anyone can create commercial projects using it, and I may do this for a company to sustain development,
@@ -53,4 +54,4 @@ import{t as p,a as c}from"../chunks/FM2UfGgQ.js";import{f as m,s as n,c as a,P a
 			learn the ropes of video communication in a low pressure environment. My other channel <a href="https://youtube.com/@webdevladder_vods">@webdevladder_vods</a> gives me even lower pressure
 			experience making videos related to my software and practices, including unfiltered programming
 			sessions for those interested in learning that particular way.</p> <p>For more about me, see my <a href="https://www.ryanatkn.com/">personal website</a>.</p> <p>If any of this sounds interesting, there's all kinds of links and buttons to click. See you
-			next time.</p></section>`,1);function _(i){f(i,{post:y,children:(d,b)=>{var e=w(),t=m(e),o=n(a(t),8),l=n(a(o));h(l,"href",`${u??""}/blog/feed.xml`),s(),r(o),r(t),s(4),c(d,e)},$$slots:{default:!0}})}export{_ as component};
+			next time.</p></section>`,1);function _(i){f(i,{post:y,children:(d,b)=>{var e=w(),t=c(e),o=n(a(t),8),l=n(a(o));h(l,"href",`${u??""}/blog/feed.xml`),s(),r(o),r(t),s(4),m(d,e)},$$slots:{default:!0}})}export{_ as component};
