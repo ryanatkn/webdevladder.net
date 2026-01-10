8301d58a49984a4d0ebd78dc3f570cea30938055
Ryan Atkinson <mail@ryanatkn.com>
2026-01-10 06:20:00 -0500

    wip

--- src/routes/FootnoteContent.svelte
+++ src/routes/FootnoteContent.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <div class="mb_xl5" id="footnote_{number}">
-	<span class="font_family_mono float_left mr_sm"
+	<span class="font_family_mono float:left mr_sm"
 		><a href="#footnote_link_{number}">[{number}]</a></span
 	>
 	{@render children()}
