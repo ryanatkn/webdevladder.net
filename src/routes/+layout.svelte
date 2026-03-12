d0fafd7093c8af865969b10630219876f6cc3ba6
Ryan Atkinson <mail@ryanatkn.com>
2026-03-12 08:51:41 -0400

    upgrade dev deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import '@fuzdev/fuz_code/theme.css';
 	import '$routes/style.css';
 
-	import Themed from '@fuzdev/fuz_ui/Themed.svelte';
+	import ThemeRoot from '@fuzdev/fuz_ui/ThemeRoot.svelte';
 	import type {Snippet} from 'svelte';
 	import {blog_feed_context} from '@fuzdev/fuz_blog/blog.js';
 
@@ -23,6 +23,6 @@
 	<link rel="alternate" type="application/atom+xml" title="Atom" href={feed.atom.feed_url} />
 </svelte:head>
 
-<Themed>
+<ThemeRoot>
 	{@render children()}
-</Themed>
+</ThemeRoot>
